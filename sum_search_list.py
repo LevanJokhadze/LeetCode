d95bda19f0cd056c83af5afd473d4caf1c47af5b
Levan <156325591+LevanJokhadze@users.noreply.github.com>
2024-01-16 17:10:46 +0400

    Sum of 2 search in list when have target

---
+++ sum_search_list.py
@@ -0,0 +1,16 @@
+class Solution:
+    def __init__(self):
+        self.arr = list()
+    def search(self, arr, targ):
+        leng = len(arr)
+        for keys in range(leng):
+            for i in range(leng):
+                temp = arr[keys] + arr[i]
+                if temp == targ:
+                    self.arr.append(keys)
+                    self.arr.append(i)
+                    return self.arr
+
+
+init = Solution()
+print(init.search([2, 7, 11, 5], 9))
